84d33c71a013e0b48aba4caa694ad46a12c7c9fa
Jorge Luiz <jorgeluiz@Jorges-MacBook-Pro.local>
2019-07-27 14:35:46 -0300

    added 2 new pages

---
+++ js/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // $('.dropdown-toggle').click(function (e) {
+    //     let parentDiv = $('.dropdown-toggle').parent();
+    //     if(parentDiv.hasClass("show")){
+    //         $('.dropdown-toggle').dropdown("hide");
+    //        return;
+    //     }else{
+    //         $('.dropdown-toggle').dropdown("show");
+    //         return;
+    //     }
+    // });
+});
